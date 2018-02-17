ef1875e22d8a8b0a00f4deb8adb64d5f36e4463c
Brandon Hill <brandon.hill1025@gmail.com>
2018-02-17 16:10:51 -0700

    rewrote work to have inputs just in Main

--- JamesWolfLab3 GranolaBar/GranolaBar.cs
+++ JamesWolfLab3 GranolaBar/GranolaBar.cs
@@ -10,69 +10,38 @@ namespace JamesWolfLab3_GranolaBar
     {
         static void Main(string[] args)
         {
-            
             DisplayGreetings();
 
+            Console.WriteLine("How many cases did you sell?");
+            int stuCasesSold = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What price did you sell each bar?\n(Please enter in decimal format ie 5.00): ");
+            decimal stuBarPrice = Convert.ToDecimal(Console.ReadLine());
+
+            int barsSold = CalculateBars(stuCasesSold);
+            Console.WriteLine("Number of actual bars sold {0}\n", barsSold);
+
+            decimal totalPriceAfterTax = CalculateTotalBPrice(barsSold, stuCasesSold, stuBarPrice);
+            Console.WriteLine("You made {0:c} before Student Government Theft", totalPriceAfterTax);
             Console.ReadKey();
-
-            
-
-            DisplayInputBars();
-            Console.ReadKey();
-
-            GetPriceBar();
-            Console.ReadKey();
-
-
-
         }
 
         public static void DisplayGreetings()
         {
-            Console.WriteLine("Welcome Computer Culb member to the Granola Bar Calculator!\nPlease press any key to continue: ");
-            
-        }
-
-        public static int GetCasesSold()
-        {
-            Console.WriteLine("How many cases did you sell?");
-            int stuCasesSold = Convert.ToInt32(Console.ReadLine());
-            return stuCasesSold;
-            
+            Console.WriteLine("Welcome Computer Club member to the Granola Bar Calculator!\nPlease press any key to continue:");
+            Console.ReadKey();
         }
         
-        public static int ClaculateBars( int caseSold)
+        public static int CalculateBars(int caseSold)
         {
             return caseSold * 12;
-           
         }
 
-        public static decimal GetPriceBar()
+        public static decimal CalculateTotalBPrice(int barsSold, int caseSold, decimal stuBarPrice)
         {
-            Console.WriteLine("What price did sell each bar?\n(Please enter in decimal format ie 5.00) : ");
-            decimal stuBarPrice = Convert.ToDecimal(Console.ReadLine());
-            return stuBarPrice;
+            decimal convertBarsSold = Convert.ToDecimal(barsSold);
+            decimal convertCaseSold = Convert.ToDecimal(caseSold);
+            return convertBarsSold * stuBarPrice - (convertCaseSold * 5);
         }
-
-        public static int ClaculateTotalBPrice(int barsSold, int caseSold, int stuBarPrice)
-        {
-            return  barsSold * stuBarPrice - (caseSold * 5);
-        }
-    
-        public static void DisplayInputBars()
-        {
-            int stuCasesSold = GetCasesSold();
-            int barsSold = ClaculateBars(stuCasesSold);
-            Console.WriteLine("Number of actual bars sold {0}/npress any key to continue:", barsSold);
-        }
-
-        public static void DisplayTotalBeforeTax()
-        {
-            int totalPriceAfterTax = ClaculateTotalBPrice(); 
-      
-            Console.WriteLine("You made this much before Student Government Tax: {0:c} ", totalPriceAfterTax);
-        }
-
     }
-
 }
