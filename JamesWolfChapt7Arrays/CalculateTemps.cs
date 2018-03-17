6cebd58225f177a5a51286491b0d52a9c5115713
Brandon Hill <brandon.hill1025@gmail.com>
2018-03-16 19:59:52 -0600

    Removed extra DetermineHighest call

--- JamesWolfChapt7Arrays/CalculateTemps.cs
+++ JamesWolfChapt7Arrays/CalculateTemps.cs
@@ -21,7 +21,14 @@ namespace JamesWolfChapt7Arrays
 
         private void HighestCalc_Click(object sender, EventArgs e)
         {
-            OutPutDisplayCalc.Text = string.Format("The highest Temp of the week was: {0}", cb.DetermineHighest(cb.DetermineHighest(Convert.ToInt32(SundayTextBox.Text), Convert.ToInt32(MondayTextBox.Text), Convert.ToInt32(TuesdayTextBox.Text), Convert.ToInt32(WednesdayTextBox.Text), Convert.ToInt32(ThursdayTextBox.Text), (Convert.ToInt32(FridayTextBox.Text)), Convert.ToInt32(SaturdayTextBox.Text));
+            OutPutDisplayCalc.Text = string.Format("The highest Temp of the week was: {0}", (cb.DetermineHighest(
+                                                  Convert.ToInt32(SundayTextBox.Text), 
+                                                  Convert.ToInt32(MondayTextBox.Text), 
+                                                  Convert.ToInt32(TuesdayTextBox.Text), 
+                                                  Convert.ToInt32(WednesdayTextBox.Text), 
+                                                  Convert.ToInt32(ThursdayTextBox.Text), 
+                                                  Convert.ToInt32(FridayTextBox.Text), 
+                                                  Convert.ToInt32(SaturdayTextBox.Text))));
 
             //int num1, num2, num3, num4, num5, num6, num7;
             //if (int.TryParse(SundayTextBox.Text, out num1) && int.TryParse(MondayTextBox.Text, out num2) && int.TryParse(TuesdayTextBox.Text, out num3) && int.TryParse(WednesdayTextBox.Text, out num4) &&
